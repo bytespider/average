912d098adb65dee7d4bf806d933adc6fc86cc616
Rob Griffiths <rob@bytespider.dev>
2021-02-04 09:28:37 +0000

    use reduce so that the function can be used with typed arrays

--- src/average.js
+++ src/average.js
@@ -1,6 +1,5 @@
 module.exports = function average(values) {
-    for (var i = 0, sum = 0, l = values.length; i < l; ++i) {
-        sum += values[i];
-    }
-    return sum / l;
+    return values.reduce(function (previousValue, currentValue) {
+        return previousValue + currentValue
+    }) / values.length
 };
